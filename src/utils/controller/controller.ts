4c9bfae80c1f2ead591a7cebda54ddb4e121069e
Igor <igor.motorin@gmail.com>
2023-02-13 05:19:46 +0300

    init: version  MVC

--- src/utils/controller/controller.ts
+++ src/utils/controller/controller.ts
@@ -1,17 +1,26 @@
-import type { Model } from '@/components/model/model';
-import type { View } from '@/components/view';
+import { Router } from '@/utils/router';
 
-import type { Router } from '@/utils/router';
+import { Model } from '@/components/model/model';
+import { View } from '@/components/view';
 
 export class Controller {
   private router: Router;
   view: View;
   model: Model;
 
-  constructor(model: Model, view: View, router: Router) {
-    this.view = view;
-    this.router = router;
-    this.model = model;
+  constructor() {
+    this.model = new Model();
+    this.view = new View({
+      onlogin: this.model.loginUser.bind(this.model),
+      onregistration: this.model.registerUser.bind(this.model),
+      onsetting: this.model.setSettings.bind(this.model),
+      ongetuser: this.model.getUser.bind(this.model),
+      onsettransaction: this.model.setTransactions.bind(this.model),
+    });
+    this.router = new Router({
+      onupdate: this.view.main.updateMain.bind(this.view.main),
+      changePages: this.view.changePages.bind(this.view),
+    });
   }
 
   addQueryListener(): void {

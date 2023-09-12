88d424621670c2c8ce3596047ab82e1f73fa2150
vladtarnovskiy <vladtarnovski989@gmail.com>
2023-09-12 17:02:38 +0300

    refactor: input corners

--- src/components/pages/authorization/InputEmail.ts
+++ src/components/pages/authorization/InputEmail.ts
@@ -31,7 +31,7 @@ export class InputEmail extends BaseComponent {
           autocomplete: 'email',
           type: 'email',
           class:
-            'relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm',
+            'relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm',
         },
       },
     );
